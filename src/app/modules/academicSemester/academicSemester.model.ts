3107ef3061fa787d14228286f6a3bb5d7c8a7756
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-06-16 12:15:45 +0600

    Partial search implemetation done

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -19,7 +19,7 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
       enum: AcademicSemesterTitles,
     },
     year: {
-      type: Number,
+      type: String,
       required: true,
     },
     code: {
